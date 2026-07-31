9a294f6e071be4fe52b89c36727037b1f4863f28
LOOKSFilm <dev@looks.film>
2026-07-31 13:38:58 +0200

    noch mehr init

--- ES-Search.py
+++ ES-Search.py
@@ -1,53 +1,47 @@
 # --------- IMPORTS ---------
-from FUNC_LIB import get_flow_cred
+from FUNC_LIB import get_cred, make_log, write_log
 from operator import itemgetter
 import FlowAPI
 from pathlib import Path
-
-
-
-
-
-# --------- INIT ---------
-metadata_api = FlowAPI.Metadata.create_gateway_instance(
-    *itemgetter("flow_host", "flow_user", "flow_password")(get_flow_cred())
-)
-
+import datetime
+import json
 
 
 # --------- CONFIG ---------
 test_mode = True
 
-
 search_fields = {}
-result_fields = {}
-
-log_path = Path(__file__).parent / "log.csv"
+return_fields = {}
 
 
+# --------- INIT ---------
+metadata_api = FlowAPI.Metadata.create_gateway_instance(
+    get_cred("flow_user"),
+    get_cred("flow_password"),
+    get_cred("flow_host")
+    )
 
-
-# --------- GLOBALS ---------
-LIMIT = 0
-
+main_log = make_log("searcher_log.txt")
+write_log(main_log, f"{datetime.datetime.now():%Y-%m-%d %H:%M:%S}: Searcher started.")
 
 
 # --------- FUNC ---------
-def test_mode():
-    global LIMIT
-
+def set_limit():
+    """
+    Set the limit for the number of clips to retrieve based on test mode.
+    """
     if test_mode:
-        print(" ~ ~ ~ Test mode is ON. ~ ~ ~")
-        LIMIT = 10
-
+        return 1
     else:
-        LIMIT  = metadata_api.numClips()
-
-
+        return metadata_api.numClips()
 
 
 # --------- MAIN ---------
-all_clips   = metadata_api.clips(offset=0, limit=LIMIT)
-total_clips = len(all_clips)
+limit = set_limit()
+clip_ID = metadata_api.clips(offset=336549, limit=limit)
+clip_ID = [1863759]
+clip_metadata = metadata_api.getClipsByIDs(clip_ID)
+
+
 
 
--- FUNC_LIB.py
+++ FUNC_LIB.py
@@ -6,12 +6,8 @@ from pathlib import Path
 
 
 # --------- FUNC HELPER---------
-def get_cred():
-    """
-    Load Flow credentials from cred.env and set them as environment variables.
-    """
-    env_path = Path(__file__).parent / "cred.env"
-    load_dotenv(env_path)
+
+
 
 
 
@@ -19,17 +15,35 @@ def get_cred():
 
 
 # --------- FUNC MAIN---------
-def get_flow_cred():
+def get_cred(cred):
     """
-    Retrieve Flow credentials from environment variables and return them as a dictionary.
-    Usage in caller: *itemgetter("flow_host", "flow_user", "flow_password")(get_cred())
+    Load credentials from cred.env and return a single credential value.
+    Usage in Caller e.g. "get_cred("flow_host")"
     """
-    get_cred()
-    return {
-        "flow_host": os.environ.get("FLOW_HOST"),
-        "flow_user": os.environ.get("FLOW_USER"),
-        "flow_password": os.environ.get("FLOW_PASSWORD"),
-    }
+    env_path = Path(__file__).parent / "cred.env"
+    load_dotenv(env_path)
+
+    if cred == "flow_host":
+        return os.environ.get("FLOW_HOST")
+    elif cred == "flow_user":
+        return os.environ.get("FLOW_USER")
+    elif cred == "flow_password":
+        return os.environ.get("FLOW_PASSWORD")
 
 
 
+def make_log(log_name="log.txt"):
+    """
+    Create a log file in the same directory as this script.
+    """
+    log_path = Path(__file__).parent / log_name
+    log_path.touch(exist_ok=True)
+    return log_path
+
+
+def write_log(log_path, message):
+    """
+    Write a message to a log file.
+    """
+    with open(log_path, "a") as log_file:
+        log_file.write(message + "\n")
