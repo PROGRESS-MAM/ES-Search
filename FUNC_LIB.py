e8d7d9bede6ee3da9ce730100b6508f25a6af0a5
LOOKSFilm <dev@looks.film>
2026-07-31 12:00:56 +0200

    init

--- FUNC_LIB.py
+++ FUNC_LIB.py
@@ -1,15 +1,30 @@
+# --------- IMPORTS ---------
 import os
 from dotenv import load_dotenv
 from pathlib import Path
 
 
-def get_flow_cred():
+
+# --------- FUNC HELPER---------
+def get_cred():
     """
-    Load Flow credentials from cred.env and return them as a dict.
-    Usage in caller: *itemgetter("flow_host", "flow_user", "flow_password")(get_cred())
+    Load Flow credentials from cred.env and set them as environment variables.
     """
     env_path = Path(__file__).parent / "cred.env"
     load_dotenv(env_path)
+
+
+
+
+
+
+# --------- FUNC MAIN---------
+def get_flow_cred():
+    """
+    Retrieve Flow credentials from environment variables and return them as a dictionary.
+    Usage in caller: *itemgetter("flow_host", "flow_user", "flow_password")(get_cred())
+    """
+    get_cred()
     return {
         "flow_host": os.environ.get("FLOW_HOST"),
         "flow_user": os.environ.get("FLOW_USER"),
